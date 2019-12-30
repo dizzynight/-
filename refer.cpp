a8a483069af424ea4315874373d3e36c6728838a
unknown <1971338325@qq.com>
2019-12-30 10:11:28 +0800

    544566

--- refer.cpp
+++ refer.cpp
@@ -4,10 +4,10 @@
 #include<string>
 #include<stdio.h>
 using namespace std;
-
 void refer()   //查询某个路由器的路由表
 {
 	stringstream st;
+
 	build();
 	cout << "请输入你想要求的路由器的路由表" << endl;
 	int t;
@@ -15,19 +15,35 @@ void refer()   //查询某个路由器的路由表
 	int z;
 	for (z = 1; z <= d; z++)
 	{
-		if (z != t)
+		if (code[t][z] != 999)
+			break;
+	}
+	if (z != d)
+	{
+		for (z = 1; z <= d; z++)
 		{
-			if (text[t][z] < 999)
+			if (z != t)
 			{
-				cout << "目的路由器为" << "    " << z  << "    ";
-				cout <<  "最短距离为" << "    ";
-				cout << text[t][z] <<"    ";
-				cout << "下一跳路由器为" <<"    ";
-				cout << path[t][z] << endl;
+				if (text[t][z] < 999)
+				{
+					cout << "目的路由器为" << "    " << z << "    ";
+					cout << "最短距离为" << "    ";
+					cout << text[t][z] << "    ";
+					cout << "下一跳路由器为" << "    ";
+					cout << path[t][z] << endl;
+				}
+				else {
+					cout << "路由器" << t << "无法到达路由器" << z << endl;
+				}
 			}
 			else {
-				cout << "路由器" << t << "无法到达路由器" << z << endl;
+				cout << "目的路由器为" << "    " << z << "    ";
+				cout << "最短距离为" << "    ";
+				cout << "0" << "    ";
+				cout << "下一跳路由器为" << "    ";
+				cout << z << endl;
 			}
 		}
 	}
+	else cout << "该路由器不存在" << endl;
 }
