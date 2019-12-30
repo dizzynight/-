7c15e26832cb0bb54ce0a817a25abd4ed70d1450
unknown <1971338325@qq.com>
2019-12-30 12:24:09 +0800

    5441144

--- refer.cpp
+++ refer.cpp
@@ -7,10 +7,11 @@ using namespace std;
 void refer()   //查询某个路由器的路由表
 {
 	stringstream st;
-
+	int o[20];
 	build();
 	cout << "请输入你想要求的路由器的路由表" << endl;
 	int t;
+	int l;
 	cin >> t;
 	int z;
 	for (z = 1; z <= d; z++)
@@ -20,30 +21,60 @@ void refer()   //查询某个路由器的路由表
 			break; 
 		}	
 	}
+	int p = 0;
 	if (z != d+1&&z<=d+1&&t<=d)
 	{
 		for (z = 1; z <= d; z++)
 		{
-			if (z != t)
+
+			for (int c = 1; c <= d; c++)
 			{
-				if (text[t][z] < 999)
+				
+				l = 0;
+				for (int f = 1; f <= d; f++)
 				{
-					cout << "目的路由器为" << "    " << z << "    ";
-					cout << "最短距离为" << "    ";
-					cout << text[t][z] << "    ";
-					cout << "下一跳路由器为" << "    ";
-					cout << path[t][z] << endl;
-				}
-				else {
-					cout << "路由器" << t << "无法到达路由器" << z << endl;
+					if (code[c][f] == 999)
+					{
+						l = l + 1;
+					}
+					if (l == d)
+					{
+						o[p] = c;
+						p = p + 1;
+					}
 				}
 			}
-			else {
-				cout << "目的路由器为" << "    " << z << "    ";
-				cout << "最短距离为" << "    ";
-				cout << "0" << "    ";
-				cout << "下一跳路由器为" << "    ";
-				cout << z << endl;
+			int m = 0;
+			for (p = 0; p <= d; p++)
+			{
+				if (z == o[p])
+				{
+					m = m + 1;
+				}
+			}
+			if (m != 1)
+			{
+				if (z != t)
+				{
+					if (text[t][z] < 999)
+					{
+						cout << "目的路由器为" << "    " << z << "    ";
+						cout << "最短距离为" << "    ";
+						cout << text[t][z] << "    ";
+						cout << "下一跳路由器为" << "    ";
+						cout << path[t][z] << endl;
+					}
+					else {
+						cout << "路由器" << t << "无法到达路由器" << z << endl;
+					}
+				}
+				else {
+					cout << "目的路由器为" << "    " << z << "    ";
+					cout << "最短距离为" << "    ";
+					cout << "0" << "    ";
+					cout << "下一跳路由器为" << "    ";
+					cout << z << endl;
+				}
 			}
 		}
 	}
